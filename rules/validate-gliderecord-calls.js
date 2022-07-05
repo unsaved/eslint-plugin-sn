b0431500562dd1c658a411ce8ca4feb817f82201
Blaine Simpson <blaine.simpson@admc.com>
2022-07-05 11:01:29 -0400

    Implemented rule validate-gliderecord-calls

---
+++ rules/validate-gliderecord-calls.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const message =
+  "Check return values of GlideRecord insert, update, get, next, deleteRecord calls.  "
+  + "This can detect many common runtime problems.";
+const messageId =  // eslint-disable-next-line prefer-template
+  (require("path").basename(__filename).replace(/[.]js$/, "") + "_msg").toUpperCase();
+
+const GR_CHECK_FNS = ["insert", "update", "get", "deleteRecord", "next", "_next"];
+const grVars = [];
+const checkInstantiator = (astId, newExpr) => {
+    /*console.debug("Called cI adding: " +
+      (astId.type === "Identifier"
+      && newExpr.callee !== undefined && newExpr.callee.type === "Identifier"
+      && newExpr.callee.name === "GlideRecord" ? astId.name : "<nul>")); */
+    if (astId.type === "Identifier"
+      && newExpr.callee !== undefined && newExpr.callee.type === "Identifier"
+      && newExpr.callee.name === "GlideRecord") grVars.push(astId.name);
+};
+
+const esLintObj = {
+    meta: {
+        type: "problem",
+        docs: {
+            description:
+              "Require checking GlideRecord insert, update, get, next, deleteRecord return values.",
+            category: "Possible Problems",
+        },
+        /*
+        schema: [{
+            type: "object",
+            properties: {
+                grFactoryFunctions: {
+                    type: "array",
+                    items: {
+                        type: "string"
+                    },
+                    uniqueItems: true
+                },
+            },
+            additionalProperties: false
+        }],
+        */
+        messages: { },
+    },
+
+    create: context => { return {
+        CallExpression: (node) => {
+            /*console.info("CE: " + ((node.callee && node.callee.object
+              && node.callee.property && grVars.includes(node.callee.object.name))
+              ? node.callee.property.name : "<nil>"));*/
+            if (!node.callee || !node.callee.object || !node.callee.property
+              || !grVars.includes(node.callee.object.name)
+              || !GR_CHECK_FNS.includes(node.callee.property.name)) return;
+            // Good if CallExpression parent is IfStatement or SwitchStatement
+            if (node.parent.type !== "ExpressionStatement") return;
+            switch(node.parent.parent.type) {
+                case "Program":
+                case "BlockStatement":
+                    context.report({node, messageId});
+                    break;
+                // purposefully no default
+            }
+        },
+        VariableDeclarator: node => {
+            if (node.id && node.init && node.init.type === "NewExpression")
+                checkInstantiator(node.id, node.init);
+        }, AssignmentExpression: node => {
+            if (node.left && node.right && node.right.type === "NewExpression")
+                checkInstantiator(node.left, node.right);
+        }
+    }; }
+};
+esLintObj.meta.messages[messageId] = message;
+module.exports = esLintObj;
---
+++ test/validate-gliderecord-calls-test.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const baseName = require("path").basename(__filename).replace(/-test[.]js$/, "");
+const messageId = (baseName + "_msg").toUpperCase();  // eslint-disable-line prefer-template
+new (require("eslint").RuleTester)().run(baseName, require(`../rules/${baseName}`), {
+    valid: [
+        'var gr = new GlideRecord("x"); var retval = gr.next()',
+        'var r;\nr = new GlideRecord("x"); if (r._next()) console.log("good");',
+    ],
+    invalid: [
+        {
+            code: 'var gr = new GlideRecord("x"); gr.next()',
+            errors: [{messageId}],
+        },
+        {
+            code: 'var r;\nr = new GlideRecord("x"); r._next()',
+            errors: [{messageId}],
+        },
+        {
+            code: 'var unrelated = 3;\n\nfunction other() {\n'
+              + '    var r;\n    r = new GlideRecord("x");\n    r.deleteRecord()\n}',
+            errors: [{messageId}],
+        },
+        {
+            code: 'var unrelated = 3;\n\nvar fn = function() {\n'
+              + '    var r;\n    r = new GlideRecord("x");\n    r.insert()\n};',
+            errors: [{messageId}],
+        },
+        {
+            code: 'if (true) {\nvar r = new GlideRecord("x"); r.update()\n}',
+            errors: [{messageId}],
+        },
+        {
+            code: 'if (true) {\nvar r; r = new GlideRecord("x"); r.get()\n}',
+            errors: [{messageId}],
+        },
+    ]
+});
