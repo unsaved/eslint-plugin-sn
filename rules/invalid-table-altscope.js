d59fc7077bb0eb1fc138e70e6429d2ce3e99ebc3
Blaine Simpson <blaine.simpson@admc.com>
2022-07-12 08:51:21 -0400

    Grealy improved invalid-table-altscope rule failure message

--- rules/invalid-table-altscope.js
+++ rules/invalid-table-altscope.js
@@ -1,7 +1,12 @@
 "use strict";
 
+/**
+ * This rule ALWAYS fails when it is executed
+ */
+const ALTSCOPES = ["global", "scoped", "iso", "noniso",
+  "iso_globalaction", "noniso_globalaction", "iso_scopedaction", "noniso_scopedaction"];
 const message =
-  "Invalid table/scope combo";
+`Invalid table/scopealt combo '{{table}}/{{scopealt}}'.  Scopealt possibilities: ${ALTSCOPES}`;
 const messageId =  // eslint-disable-next-line prefer-template
   (require("path").basename(__filename).replace(/[.]js$/, "") + "_msg").toUpperCase();
 const esLintObj = {
@@ -15,12 +20,21 @@ const esLintObj = {
     },
 
     create: context => {
-        let beenCalled = false;
         return {
             onCodePathStart: (codePath, node) => {
-                if (beenCalled) return;
-                beenCalled = true;
-                context.report({node, messageId});
+                if (node.type !== "Program") return;
+                const ex = /([^/]+)[/]([^/]+)[/][^/]+[.]js/.exec(context.getFilename());
+                if (!ex) throw new Error(`Malformatted ESLint filename: ${context.getFilename()}`);
+                let t, a;
+                if (ALTSCOPES.includes(ex[2])) {
+                    t = ex[1]; a = ex[2];
+                } else {
+                    t = ex[2]; a = '<NONE>';
+                }
+                context.report({node, messageId, data: {
+                    table: t,
+                    scopealt: a
+                }});
             }
         };
     }
