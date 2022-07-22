e35e1341331b1b91d15d94a16156bc4295a2ec63
Blaine Simpson <blaine.simpson@admc.com>
2022-07-22 12:40:06 -0400

    Added gather script that missed adding to previous commit

---
+++ resources/gatherSiNames.js
@@ -0,0 +1,17 @@
+/**
+ * Can run this as a bg script or in a fix script
+ */
+
+/* eslint-disable strict, prefer-template, no-var */
+/* global GlideAggregate, gs */
+const gatherScoped = false;  // TODO:  Set this to true to gather globals; to false for scoped
+var n;
+const ga = new GlideAggregate("sys_script_include");
+ga.addQuery("sys_scope.scope", gatherScoped ? "!=" : "=", "global");
+ga.addQuery("active", true);  // Comment out statement to allow for enabling active==false's
+ga.orderBy("name");
+ga._query();
+const names = [];
+while (ga._next()) { n = ga.getValue("name"); if (/^[_a-zA-Z]\w*$/.test(n)) names.push(n); }
+gs.log(names.length + " " + (gatherScoped ? "scoped" : "global")
+  + " names\n" + names.join("\n"), "siScopeList");
