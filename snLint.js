b6bb07ae0b0da7a3ce5111770161c6844b879152
Blaine Simpson <blaine.simpson@admc.com>
2024-01-12 19:32:56 -0500

    Fix to ensure file termination when wrapping

--- snLint.js
+++ snLint.js
@@ -183,8 +183,8 @@ function lintFile(file, table, alt, readStdin=false) {
          * Without this, this hack gives false positive if there are root-level statements
          * between the function definition and bottom of script. */
         if (RAWFN_TEST_PAT.test(justCode)) {
-            // 'api.ctoller=function(...' is what you get with OOTB boilerplate.
-            content = content.replace(RAWFN_SUB_PAT, "api.controller=function(") + ";";
+            // 'api.controller=function(...' is what you get with OOTB boilerplate.
+            content = content.replace(RAWFN_SUB_PAT, "api.controller=function(") + ";\n";
             console.warn("Inserted dummy assignment before Angular anonymous function");
         }
     } else if (table === "sa_pattern") {
