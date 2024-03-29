4a522aaddc38fa7ded983cec532201bc878a1b4b
Blaine Simpson <blaine.simpson@admc.com>
2024-03-29 15:18:05 -0400

    Documented and updated validation scripts

---
+++ resources/sys_script_validator/script-validator_desktop.js
@@ -0,0 +1,25 @@
+function validate(value) {
+    "use strict";
+    /* eslint-disable prefer-arrow-callback, prefer-template */
+    /* Blaine fixes this:
+    if (g_form.getTableName() === 'sys_ui_script' || g_form.getTableName() === 'sys_script_client')
+        return true; */
+    switch (g_form.getTableName()) {
+        case "sys_ui_action":
+        case "sp_widget":
+        case "sys_ux_data_broker_scriptlet":
+        case "sys_ux_data_broker_transform":
+        case "sys_ui_context_menu":
+            return true;
+    }
+
+    const ajax = new GlideAjax('JSValidator');
+    const scope = g_form.getValue('sys_scope');
+    ajax.addParam('sysparm_name', 'validate');
+    ajax.addParam('sysparm_js_expression', value);
+    ajax.addParam('sysparm_scope', scope);
+    ajax.getXMLWait();
+    const answer = ajax.getAnswer();
+    if (answer === null) return true;
+    return getMessage("Could not save record because of a compile error: " + answer);
+}
